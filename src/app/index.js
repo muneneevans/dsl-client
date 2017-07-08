5cb2b07b29a05de055f9ed7400f6f17d87c65ff0
Evans Munene <munene.evansk@gmail.com>
2017-07-08 20:13:02 +0300

    Set App component as default in index.js

--- src/app/index.js
+++ src/app/index.js
@@ -4,12 +4,11 @@ import {Provider} from "react-redux"
 import 'babel-polyfill';  
 
 import App from "./Containers/App"
-import MapScreen from "./Containers/MapScreen"
 import store from './Store/configureStore'
 
 render(
     <Provider store={store}>
-        <MapScreen/>
+        <App/>
     </Provider>,
     document.getElementById('app')
 );
