a84332bb365991dd40e16a89f024424f573ffd34
Evans Munene <munene.evansk@gmail.com>
2017-07-08 20:18:26 +0300

    Add: Facility reducer

--- src/app/Store/Facilities/reducer.js
+++ src/app/Store/Facilities/reducer.js
@@ -1,11 +1,31 @@
-const nameInitialState = {}
-const name = (state = nameInitialState, action) => {
+import * as types from "./actionTypes"
+import Immutable from "seamless-immutable"
+
+const InitialState = Immutable({
+    countyIds: undefined
+})
+
+export default function facilityReducer(state = InitialState, action={}){
     switch (action.type) {
-        case ACTION_TYPE_1:
-            return state
-        case ACTION_TYPE_2:
-            return state
+        case types.COUNTY_CODES_FETCHED:                        
+            return state.merge({
+                countyIds: action.countyIds
+            })
+        case types.COUNTY_CONSTITUENCY_CODES_FETCHED:
+            return state.merge({
+                constituencyCodes: action.constituencyCodes
+            })
         default:
             return state
     }
+}
+
+
+//selectors
+export function getCountyIds(state){
+    return state.facilityReducer.countyIds
+}
+
+export function getCountyConstituencyCodes(state){
+    return state.facilityReducer.constituencyCodes
 }
