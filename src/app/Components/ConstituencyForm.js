c51c3d5ebfa089e0819d41a58e7991fd4c4bf70a
Evans Munene <munene.evansk@gmail.com>
2017-07-28 17:28:40 +0300

    Add: Constituency Form

---
+++ src/app/Components/ConstituencyForm.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Form, Segment, Input } from 'semantic-ui-react'
+
+export const ConstituencyForm = (props) => {
+    
+    const handleChange = (event) => {
+        // alert(event.target.value)
+        props.fetchCountyConstituencyCodes(event.target.value)
+    }
+
+    const handleConstituencySelect = (event) =>{
+        props.fetchConstituencyWardCodes(event.target.value)
+    }
+
+    return (
+        <Form>
+            <Form.Field>
+                <label>County</label>
+                <select
+                    onChange={handleChange.bind(this)}
+                    placeholder='select a county'>
+                    {
+                        props.countyCodes.map((county, i) => (
+                            <option
+                                key={i}
+                                value={county.id}>
+
+                                {county.name}
+
+                            </option>
+                        ))
+                    }
+                </select>
+            </Form.Field>
+            <Form.Field>
+                <label>Consitituency</label>
+                    {props.constituencyCodesIsFetched ? (
+                        <select
+                            onChange={handleConstituencySelect.bind(this)}>
+                                {
+                                    props.constituencyCodes.map((constituency, i) => (
+                                        <option
+                                            key={i}
+                                            value={constituency.id}>
+
+                                            {constituency.name}
+                                        </option>
+                                        )
+                                    )
+                                    
+                                }
+                        </select>
+
+                    ) : (
+                        <Input 
+                            loading
+                            value='Loading'>
+                            
+                        </Input>
+                    )}
+            </Form.Field>
+        </Form>
+    )
+}
+
+export default ConstituencyForm
