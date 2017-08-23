054aee6adec89e7892b1f6bd438e73626fc882bd
Evans Munene <munene.evansk@gmail.com>
2017-08-23 08:32:30 +0300

    Add: get indicators and dataelements

--- src/app/Services/IndicatorService.js
+++ src/app/Services/IndicatorService.js
@@ -1,11 +1,26 @@
 export default class IndicatorService{
     static get host(){
         delete IndicatorService.host;
-        return IndicatorService.host = 'http://localhost:8000/';
+        return IndicatorService.host = 'http://localhost:8000/indicators/api/';
     }
 
-    static getDatalements(){
-        const url = this.host.concat('indicators/api/datalements/')
+    static getDataElementGroups(){
+        const url = this.host.concat('dataelementgroups/')
+
+        return fetch(url)
+            .then(response =>{
+                return response.json()
+            })
+            .catch(error =>{
+                throw(error)
+            })
+    }
+    
+    static getDatalements(dataElementGroupId=undefined){
+        var url = this.host.concat('datalements/')
+        if(dataElementGroupId){
+            url = this.host.concat('dataelementgroups/' + dataElementGroupId + '/dataelements/' )
+        }
 
         return fetch(url)
             .then(response =>{
@@ -15,4 +30,30 @@ export default class IndicatorService{
                 return error
             })
     }
+
+    static getIndicators(){
+        const url = this.host.concat('indicators/')
+
+        return fetch(url)
+            .then(response =>{
+                return response.json()
+            })
+            .catch(error =>{
+                throw(error)
+            })
+    }
+
+    static getIndicatorGroups(){
+        const url = this.host.concat('indicatorgroups/')
+
+        return fetch(url)
+            .then(response =>{
+                return response.json()
+            })
+            .catch(error =>{
+                throw(error)
+            })
+    }
+
+
 }
