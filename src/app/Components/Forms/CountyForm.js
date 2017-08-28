4d9aa702b3aa4c124a6812c94e6c60a5807a3d50
Evans Munene <munene.evansk@gmail.com>
2017-08-28 10:59:22 +0300

    Moved forms to Forms folder

---
+++ src/app/Components/Forms/CountyForm.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Card, Form } from 'semantic-ui-react'
+
+export const CountyForm = (props) => {
+
+    const handleChange = (event) => {
+        // alert(event.target.value)
+        // props.fetchCountyConstituencyCodes(event.target.value)
+        // props.fetchCountyFacilities(event.target.value)
+        // props.fetchCountySummary(event.target.value)
+        props.submitAction(event.target.value)
+    }
+
+    return (
+
+        <Form>
+            <Form.Field>
+                <label>County</label>
+                <select
+                    onChange={handleChange.bind(this)}
+                    placeholder='select a county'>
+                    {
+                        props.countyCodes.map((county, i) => (
+                            <option key={i} value={county.id}
+                            >
+                                {county.name}
+                            </option>
+                        ))
+                    }
+                </select>
+            </Form.Field>
+        </Form>
+
+    )
+}
+
+export default CountyForm
