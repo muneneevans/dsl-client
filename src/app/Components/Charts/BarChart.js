6972e5cc8fa7f4be0087d485629f92cc632d2803
Evans Munene <munene.evansk@gmail.com>
2017-09-04 14:58:41 +0300

    Update: working bar chart, no axis

--- src/app/Components/Charts/BarChart.js
+++ src/app/Components/Charts/BarChart.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { scaleLinear, scaleOrdinal } from "d3-scale"
+import { scaleLinear, scaleOrdinal, scaleBand } from "d3-scale"
 import { max } from "d3"
+import { range} from "d3-array"
 export const BarChart = ({ data, width, height }) => {
 
     var data = [
@@ -24,11 +25,11 @@ export const BarChart = ({ data, width, height }) => {
 
     var transform = `translate( ${margins.left}, ${margins.top})`
 
-    var x = scaleOrdinal()
+    var x = scaleBand()
         .domain(data.map((d, i) => {
             return d.label
         }))
-        .range([0, width])
+        .rangeRound([0, width]).padding(0.1)
 
     var y = scaleLinear()
         .domain([0, max(data, function (d) {
@@ -53,35 +54,44 @@ export const BarChart = ({ data, width, height }) => {
     }
 
     const rectForeground = (data) => {
-        // alert('going')
-        data.map((d, i) => (
-            <rect
-                fill="#74d3eb"
-                key={i}
-                x={x(d.month)}
-                y={y(d.value)}
-                height={figureHeight - y(d.value)}
-                width={x.range()} />
-
-        ))
+        data.map((d, i) => (            
+                <rect
+                    fill="#74d3eb"
+                    key={i}
+                    x={x(d.label)}
+                    y={y(d.value)}
+                    height={figureHeight - y(d.value)}
+                    width={figureHeight} />
+            ))
 
     }
     const say = () => {
         return (
-            <h1> data </h1>
+            <h1> the say function </h1>
         )
     }
     return (
-        // <svg width={figureWidth} height={figureHeight}>
-        //     <g transform={transform}>
-        //         {say()}
-        //         {rectBackground(data)}
-        //         {rectForeground(data)}
-        //     </g>
-        // </svg>
-        <div>
-            {say()}
-        </div>
+        <svg width={figureWidth} height={figureHeight}>
+            <g transform={transform}>
+                {/* {say()} */}
+                {/* {rectBackground(data)} */}
+                {/* {rectForeground(data)} */}
+                {
+                    data.map((d, i) => (
+                        <rect
+                            fill="blue"
+                            key={i}
+                            x={x(d.label)}
+                            y={y(d.value)}
+                            height={figureHeight - y(d.value)}
+                            width={ 20} />
+                    ))
+                }
+            </g>
+        </svg>
+        // <div>
+        //     {say()}
+        // </div>
     )
 }
 
