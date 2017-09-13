d341926b5dbaa144a8471abef794c7435e2d9553
Evans Munene <munene.evansk@gmail.com>
2017-09-13 14:11:53 +0300

    Update: minor update on react chart

--- src/app/Components/Charts/StackedBarChart.js
+++ src/app/Components/Charts/StackedBarChart.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Segment } from "semantic-ui-react"
 // import 'react-vis/dist/style.css';
-import { XYPlot, LineSeries, VerticalGridLines, HorizontalGridLines, YAxis, XAxis, VerticalBarSeries, HorizontalBarSeries } from 'react-vis';
+import { XYPlot, LineSeries, VerticalGridLines, HorizontalGridLines, YAxis, XAxis, VerticalBarSeries, HorizontalBarSeries, Crosshair } from 'react-vis';
 
 export const StackedBarChart = ({ dataExists, data, title, height, width, labels }) => {
-    const margins = { left: 70, right: 10, top: 10, bottom: 40 }    
+    const margins = { left: 70, right: 10, top: 10, bottom: 40 }
     return (
         <XYPlot height={height} width={width} margin={margins} >
             <XAxis title={title} />
-            <YAxis tickFormat={v => labels[v]}/>
-            <HorizontalBarSeries data={data} animation/>
+            <YAxis tickFormat={v => labels[v]} />
+            <HorizontalBarSeries data={data} animation >
+            </HorizontalBarSeries>            
         </XYPlot>
     )
 }
