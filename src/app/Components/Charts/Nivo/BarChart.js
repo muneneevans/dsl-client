20e4b0d6bfd57090d31428e719a6540f026e8c1c
Evans Munene <munene.evansk@gmail.com>
2017-09-27 16:04:09 +0300

    Add: Nivor bar chart

---
+++ src/app/Components/Charts/Nivo/BarChart.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import { Bar } from 'nivo'
+
+
+export const BarChart = (props) => {
+    return (
+        <Bar
+            data={props.data}
+            keys={props.keys}
+            indexBy="country"
+            height={props.height}
+            width={props.width}
+            margin={{
+                "top": 50,
+                "right": 60,
+                "bottom": 50,
+                "left": 60
+            }}
+            padding={0.2}
+            innerPadding={0}
+            minValue="auto"
+            maxValue="auto"
+            groupMode="stacked"
+            layout="horizontal"
+            reverse={false}
+            colors="nivo"
+            colorBy="id"
+            defs={[
+                {
+                    "id": "dots",
+                    "type": "patternDots",
+                    "background": "inherit",
+                    "color": "#38bcb2",
+                    "size": 4,
+                    "padding": 1,
+                    "stagger": true
+                },
+                {
+                    "id": "lines",
+                    "type": "patternLines",
+                    "background": "inherit",
+                    "color": "#eed312",
+                    "rotation": -45,
+                    "lineWidth": 6,
+                    "spacing": 10
+                }
+            ]}
+            fill={[
+                {
+                    "match": {
+                        "id": "fries"
+                    },
+                    "id": "dots"
+                },
+                {
+                    "match": {
+                        "id": "sandwich"
+                    },
+                    "id": "lines"
+                }
+            ]}
+            borderRadius={0}
+            borderWidth={0}
+            borderColor="inherit:darker(1.6)"
+            axisBottom={{
+                "orient": "bottom",
+                "tickSize": 5,
+                "tickPadding": 5,
+                "tickRotation": 0,
+                "legend": "country",
+                "legendPosition": "center",
+                "legendOffset": 36
+            }}
+            axisLeft={{
+                "orient": "left",
+                "tickSize": 5,
+                "tickPadding": 5,
+                "tickRotation": 0,
+                "legend": "food",
+                "legendPosition": "center",
+                "legendOffset": -40
+            }}
+            enableGridX={false}
+            enableGridY={true}
+            enableLabel={true}
+            labelSkipWidth={12}
+            labelSkipHeight={12}
+            labelTextColor="inherit:darker(1.6)"
+            animate={true}
+            motionStiffness={90}
+            motionDamping={15}
+            isInteractive={true}
+        />
+    )
+}
+
+export default BarChart
--- src/app/Containers/ChartScreen.js
+++ src/app/Containers/ChartScreen.js
@@ -5,11 +5,12 @@ import BarChart from "../Components/Charts/BarChart"
 // import DotChart from "../Components/Charts/DotChart"
 import LineChart from "../Components/Charts/LineChart"
 import HorizontalBarChart from "../Components/Charts/HorizontalBarChart"
+import NivoBarChart from "../Components/Charts/Nivo/BarChart"
 
 // const kenya = require('../Components/Charts/kenya.json')
 
 export const ChartScreen = (props) => {
-    
+
     var data = [
         { label: '02-11-2016', value: 180 },
         { label: '02-12-2016', value: 250 },
@@ -36,7 +37,15 @@ export const ChartScreen = (props) => {
         { label: 'Nov', value: 60 },
         { label: 'Dec', value: 75 }
     ]
-    
+    let Nivodata = [{"country":"AD","hot dog":191,"burger":102,"sandwich":89,"kebab":181,"fries":154,"donut":92},{"country":"AE","hot dog":55,"burger":168,"sandwich":13,"kebab":104,"fries":130,"donut":26},{"country":"AF","hot dog":0,"burger":20,"sandwich":181,"kebab":174,"fries":53,"donut":122},{"country":"AG","hot dog":133,"burger":52,"sandwich":40,"kebab":131,"fries":103,"donut":24},{"country":"AI","hot dog":119,"burger":67,"sandwich":50,"kebab":196,"fries":197,"donut":82},{"country":"AL","hot dog":175,"burger":164,"sandwich":195,"kebab":118,"fries":165,"donut":15},{"country":"AM","hot dog":146,"burger":123,"sandwich":45,"kebab":96,"fries":29,"donut":20}]
+    let keys = [
+        "hot dog",
+        "burger",
+        "sandwich",
+        "kebab",
+        "fries",
+        "donut"
+    ]
 
     return (
         <div>
@@ -45,12 +54,12 @@ export const ChartScreen = (props) => {
                 width={700}
                 height={400}
                 /> */}
-            <BarChart 
+            {/* <BarChart 
                 data={data2}
                 height              
                 width={700}
                 height={500}
-                />
+                /> */}
 
             {/* <HorizontalBarChart
                 data={data2}
@@ -61,6 +70,7 @@ export const ChartScreen = (props) => {
                 data={kenya}
                 height={800}
                 width={800} /> */}
+            <NivoBarChart data={Nivodata} keys={keys} height={500} width={900}/>
         </div>
     )
 }
