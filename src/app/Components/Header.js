c29b6d0b388d123b8087eab01b6e8d129ac6ee3a
Evans Munene <munene.evansk@gmail.com>
2017-10-31 16:49:32 +0300

    FIX: switched to hash router- has issues

--- src/app/Components/Header.js
+++ src/app/Components/Header.js
@@ -3,11 +3,11 @@ import { Button, Container, Menu, } from 'semantic-ui-react'
 import { Link } from "react-router-dom"
 
 const Header = () => (
-    <Menu fixed='top' size='large'>
+    <Menu fixed='top' size='massive' color='blue' tertiary inverted width={5}>
         <Container>
             
-            <Menu.Item as='a' active href='/' link={true}>Home</Menu.Item>
-            <Menu.Item as='a' href='/facilities'>Facilities</Menu.Item>
+            <Menu.Item as='a' href='/' link={true}  >Home</Menu.Item>
+            <Menu.Item as='a' href='/#/facilities'>Facilities</Menu.Item>
             <Menu.Item as='a'>Commodities</Menu.Item>
             <Menu.Item as='a'>Human Resource</Menu.Item>            
         </Container>
