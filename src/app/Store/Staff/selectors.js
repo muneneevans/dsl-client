04e2f5030f5cc951bdac68c91330ea2fcf41c782
Evans Munene <evans@localhost.localdomain>
2017-11-08 01:53:51 -0600

    ADD: staff form

--- src/app/Store/Staff/selectors.js
+++ src/app/Store/Staff/selectors.js
@@ -1,7 +1,43 @@
-export function getJobTypes(state){
+export function getJobTypes(state) {
     return state.staffReducer.jobTypes
 }
 
-export function getCadres(state){
+export function getJobTypeOptions(state) {
+    if (state.staffReducer.jobTypes) {
+        let options = []
+
+        state.staffReducer.jobTypes.map((jobType, i) => {
+            options.push({
+                key: jobType.id,
+                value: jobType.id,
+                text: jobType.name
+            })
+        })
+        return options
+    }
+    else {
+        return undefined
+    }
+}
+
+export function getCadres(state) {
     return state.staffReducer.cadres
+
+}
+
+export function getCadreOptions(state) {
+    if (state.staffReducer.cadres) {
+        let options = []
+        state.staffReducer.cadres.map((cadre, i) => {
+            options.push({
+                key: cadre.id,
+                value: cadre.id,
+                text: cadre.name
+            })
+        })
+        return options
+    }
+    else {
+        return undefined
+    }
 }
